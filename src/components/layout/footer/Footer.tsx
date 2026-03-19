24939dfc96e47ce23fd79f3ae1b2b810a5ddecd4
oppfuego <zaharenkoila19@gmail.com>
2026-03-19 19:07:26 +0200

    changes

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -8,17 +8,12 @@ import styles from "./Footer.module.scss";
 import {footerContent} from "@/resources/content";
 import {footerStyles} from "@/resources/styles-config";
 import {SmartLinkProps} from "@/types/smart-link";
-import {media} from "@/resources/media";
+import visaLogo from "@/assets/cards/visa.png";
+import mastercardLogo from "@/assets/cards/mastercard.png";
+import pciDssLogo from "@/assets/cards/pci-dss-compliant-logo-vector.svg";
 import {
-    FaApplePay,
-    FaCcAmex,
-    FaCcMastercard,
-    FaCcVisa,
-    FaFacebookF,
-    FaGooglePay,
     FaInstagram,
     FaLinkedinIn,
-    FaTwitter
 } from "react-icons/fa";
 
 const SmartLink: React.FC<SmartLinkProps> = ({
@@ -53,7 +48,7 @@ const SmartLink: React.FC<SmartLinkProps> = ({
 };
 
 const Footer: React.FC = () => {
-    const {logo, columns, contact, socials, legal} = footerContent;
+    const {logo, columns, contact, legal} = footerContent;
 
     const LegalAddress = () =>
         Array.isArray(legal?.addressLines) && legal.addressLines.length ? (
@@ -66,8 +61,27 @@ const Footer: React.FC = () => {
 
     const PaymentMethods = () => (
         <div className={styles.paymentsContent}>
-            <FaCcVisa className={styles.paymentIcon}/>
-            <FaCcMastercard className={styles.paymentIcon}/>
+            <Image
+                src={visaLogo}
+                alt="Visa"
+                className={styles.paymentCardLogo}
+                width={64}
+                height={42}
+            />
+            <Image
+                src={mastercardLogo}
+                alt="Mastercard"
+                className={styles.paymentCardLogo}
+                width={64}
+                height={42}
+            />
+            <Image
+                src={pciDssLogo}
+                alt="PCI DSS compliant"
+                className={styles.paymentComplianceLogo}
+                width={116}
+                height={42}
+            />
         </div>
     );
 
